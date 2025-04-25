d951b29b55292b9cfa707a533167a570a3677e57
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-04-25 16:50:01 -0700

    NEW: TopNavigation component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
-import './App.css'
+import TopNavigation from "./components/TopNavigation";
+
+import axios from "axios";
 
 import { useEffect } from "react";
-import axios from "axios";
+
+import "./App.css";
 
 function App() {
   useEffect(() => {
-    axios.get("http://localhost:3000/api/restaurants")
-      .then(res => console.log("✅ Data from backend:", res.data))
-      .catch(err => console.error("❌ Backend connection failed:", err));
+    axios
+      .get("http://localhost:3000/api/restaurants")
+      .then((res) => console.log("✅ Data from backend:", res.data))
+      .catch((err) => console.error("❌ Backend connection failed:", err));
   }, []);
 
   return (
     <div className="App">
+      <TopNavigation />
       <h1>MunchMatch</h1>
       <p>Testing backend connection in console...</p>
     </div>
---
+++ frontend/src/components/TopNavigation.jsx
@@ -0,0 +1,10 @@
+function TopNavigation() {
+  return (
+    <nav className="navbar">
+      <span>MunchMatch</span>
+      <p>user</p>
+    </nav>
+  );
+}
+
+export default TopNavigation;
