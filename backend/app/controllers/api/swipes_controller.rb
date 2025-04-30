931addfe19898a9df4fdb7665645630769410f07
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-04-30 16:47:42 +0900

    swipes controller modified

--- backend/app/controllers/api/swipes_controller.rb
+++ backend/app/controllers/api/swipes_controller.rb
@@ -1,2 +1,10 @@
 class Api::SwipesController < ApplicationController
+  before_action :set_dish, only: %i[create update]
+  before_action :set_user, only: %i[create update]
+
+  def index
+    @swipes = Swipe.includes(:dish).where(user_id: current_user.id).order(created_at: :desc)
+
+    render json: @swipes, include: { dish: { only: %i[id name photo_url restaurant_id description] } }
+  end
 end
--- backend/app/models/swipe.rb
+++ backend/app/models/swipe.rb
@@ -1,5 +1,5 @@
 class Swipe < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :dish
 
   validates :right_swipes, numericality: { greater_than_or_equal_to: 0 }
