5bddfbb8f8c855f3ca9cc874dbe5d8734462ff75
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-30 18:07:48 -0700

    Remove debugging code

--- frontend/src/pages/user/HomePage.jsx
+++ frontend/src/pages/user/HomePage.jsx
@@ -4,7 +4,7 @@ import Category from "../../components/Category";
 
 function HomePage({ categories }) {
   const [selectedCategory, setSelectedCategory] = useState(null);
-  console.log(selectedCategory);
+
   const handleCategorySelect = (categoryId) => {
     setSelectedCategory(categoryId);
   };
