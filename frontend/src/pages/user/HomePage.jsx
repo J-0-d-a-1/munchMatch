cb89ea11fbb512772cf112f67edbc415de64cb0c
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-30 17:49:46 -0700

    Implement logic to track selected category

--- frontend/src/pages/user/HomePage.jsx
+++ frontend/src/pages/user/HomePage.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuCard from "../../components/MenuCard";
+import Category from "../../components/Category";
 
-function HomePage() {
+function HomePage({ categories }) {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  console.log(selectedCategory);
+  const handleCategorySelect = (categoryId) => {
+    setSelectedCategory(categoryId);
+  };
+
   return (
     <div>
+      <Category
+        categories={categories}
+        onSelectCategory={handleCategorySelect}
+        selectedCategory={selectedCategory}
+      />
       <MenuCard />
     </div>
   );
