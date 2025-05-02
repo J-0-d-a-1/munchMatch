9e2acf8a6b8e9f79fe4a643f485a688c93e79ab2
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-05-02 15:55:31 +0900

    favorites_controller updated to handle non logged in user

--- backend/app/controllers/api/favorites_controller.rb
+++ backend/app/controllers/api/favorites_controller.rb
@@ -1,8 +1,12 @@
 class Api::FavoritesController < ApplicationController
   # GET api/favorites
   def index
-    favorites = current_user.favorites.includes(:restaurant)
-    render json: favorites.map(&:restaurant)
+    if !current_user
+      render json: { message: 'You need to login/signup to favorites' }, status: :unauthorized
+    else
+      favorites = current_user.favorites.includes(:restaurant)
+      render json: favorites.map(&:restaurant)
+    end
   end
 
   # POST api/favorites
